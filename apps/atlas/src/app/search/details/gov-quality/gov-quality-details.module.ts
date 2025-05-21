e9be38684b58ebb5d08ae81d31f6e835f482c9f5
sem.nikolaev <sem.nikolaev@aureliusenterprise.com>
2025-05-21 12:46:32 +0000

    feat: add desctiprion card to gov quality rule

--- apps/atlas/src/app/search/details/gov-quality/gov-quality-details.module.ts
+++ apps/atlas/src/app/search/details/gov-quality/gov-quality-details.module.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
+import { DescriptionModule } from '../../components/description/description.module';
 import { DataQualityPieModule } from '../../components/data-quality-pie/data-quality-pie.module';
 import { DetailsCardsListModule } from '../components/details-cards-list/details-cards-list.module';
 import { DetailsNavigationModule } from '../components/navigation/details-navigation.module';
@@ -14,6 +15,7 @@ import { NonCompliantCardsComponent } from './non-compliant-cards/non-compliant-
     DetailsNavigationModule,
     PropertiesModule,
     DataQualityPieModule,
+    DescriptionModule,
     DetailsCardsListModule,
   ],
   declarations: [
