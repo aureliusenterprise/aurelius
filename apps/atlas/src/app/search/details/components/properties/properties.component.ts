684487f46fa3c90be61c6ef9c4b7eaaceca0004d
stuti.nayak <stuti.nayak@aureliusenterprise.com>
2025-07-24 15:38:55 +0200

    fix: override static properties headers with collapsible component

--- apps/atlas/src/app/search/details/components/properties/properties.component.ts
+++ apps/atlas/src/app/search/details/components/properties/properties.component.ts
@@ -1,4 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+// import { Component, OnInit } from '@angular/core';
+// import { SortableTableShellConfig } from '@models4insight/components';
+// import { Observable } from 'rxjs';
+// import { DataForTable, PropertiesService } from './properties.service';
+
+// const tableConfigProperties: SortableTableShellConfig<DataForTable> = {
+//   name: { displayName: 'Key', isNarrow: true },
+//   value: { displayName: 'Value', isNarrow: true }
+// };
+
+// @Component({
+//   selector: 'models4insight-properties',
+//   templateUrl: './properties.component.html',
+//   styleUrls: ['./properties.component.scss'],
+//   providers: [PropertiesService]
+// })
+// export class PropertiesComponent implements OnInit {
+//   readonly tableConfigProperties = tableConfigProperties;
+
+//   dataForTable$: Observable<DataForTable[]>;
+
+//   isCollapsed = true; // collapsed by default
+
+//   constructor(
+//     private readonly propertiesService: PropertiesService
+//   ) { }
+
+//   ngOnInit() {
+//     this.dataForTable$ = this.propertiesService.select('propertiesList');
+//   }
+
+// }
+
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { SortableTableShellConfig } from '@models4insight/components';
 import { Observable } from 'rxjs';
 import { DataForTable, PropertiesService } from './properties.service';
@@ -14,11 +47,10 @@ const tableConfigProperties: SortableTableShellConfig<DataForTable> = {
   styleUrls: ['./properties.component.scss'],
   providers: [PropertiesService]
 })
-export class PropertiesComponent implements OnInit {
+export class PropertiesComponent implements OnInit, AfterViewInit {
   readonly tableConfigProperties = tableConfigProperties;
 
   dataForTable$: Observable<DataForTable[]>;
-
   isCollapsed = true; // collapsed by default
 
   constructor(
@@ -28,7 +60,16 @@ export class PropertiesComponent implements OnInit {
   ngOnInit() {
     this.dataForTable$ = this.propertiesService.select('propertiesList');
   }
-  toggleCollapse() {
-    this.isCollapsed = !this.isCollapsed;
-  }
+
+ngAfterViewInit() {
+  // Remove static Properties headers
+  setTimeout(() => {
+    const headers = document.querySelectorAll('h3.title.is-3');
+    headers.forEach(header => {
+      if (header.textContent?.trim() === 'Properties') {
+        (header as HTMLElement).style.display = 'none';
+      }
+    });
+  });
+}
 }
