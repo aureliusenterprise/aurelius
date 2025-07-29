ac65fae929db4d3aa200cf8bd8d54bd97d6fd357
thijs-franck <thijs.franck@aureliusenterprise.com>
2025-07-29 13:20:10 +0000

    feat: set up kafka scanner and clean up broken tests

--- apps/m4i-data-dictionary-io/m4i_data_dictionary_io/sources/kafka/create_from_kafka.py
+++ apps/m4i-data-dictionary-io/m4i_data_dictionary_io/sources/kafka/create_from_kafka.py
@@ -1,77 +1,289 @@
-from typing import List
-from m4i_atlas_core.config.config_store import ConfigStore
-from m4i_data_dictionary_io.entities.json import Collection, DataField, Dataset, Source, System
+import json
+import logging
+from typing import Callable, Dict, Generator, List, Union
+
+from confluent_kafka import Consumer, TopicCollection
+from confluent_kafka.admin import AdminClient
+from confluent_kafka.avro import loads
+from confluent_kafka.schema_registry import (
+    Schema,
+    SchemaRegistryClient,
+    SchemaRegistryError,
+)
+from m4i_data_dictionary_io.entities.json import (
+    Collection,
+    DataField,
+    Dataset,
+    System,
+    ToAtlasConvertible,
+    get_qualified_name,
+)
 from m4i_data_dictionary_io.functions.create_from_excel import get_ref_and_push
-from m4i_data_dictionary_io.entities.json import get_qualified_name
-from m4i_data_dictionary_io.sources.kafka.discover_cluster import discover_cluster
+
+SCHEMA_REGISTRY_MAGIC_BYTE = b"\x00"
 
 
-def create_source(name: str) -> Source:
-  return Source.from_dict({
-    "name": name,
-    "qualifiedName": name
-  })
+def get_cluster_id(admin_client: AdminClient) -> Union[str, None]:
+    """Retrieve the cluster ID from the Kafka admin client."""
+    future = admin_client.describe_cluster()
 
-def create_system(name: str, qualified_name: str) -> System:
-  return System.from_dict({
-    "name": name,
-    "qualifiedName": qualified_name
-  })
+    return (
+        cluster_metadata.cluster_id if (cluster_metadata := future.result()) else None
+    )
 
-def create_collection(name: str, system_qualified_name: str, qualified_name: str) -> Collection:
-  return Collection.from_dict({
-    "name": name,
-    "system": system_qualified_name,
-    "qualifiedName": qualified_name,
-  })
 
-def process_topic(item, collection_qualified_name: str) -> List:
-  """Process each topic by creating dataset and field instances."""
+def get_external_topic_names(admin_client: AdminClient) -> List[str]:
+    """
+    Retrieve topic names for the given cluster ID,
+    filtering out internal and default topics.
+    """
+    all_topics = [str(t) for t in admin_client.list_topics().topics.values()]
 
-  elements = []
+    futures = admin_client.describe_topics(TopicCollection(all_topics))
 
-  dataset_qualified_name = collection_qualified_name + "--" + get_qualified_name(item["name"])
-  # Create topic
-  elements.append(Dataset.from_dict({
-    "name": item["name"],
-    "collection": collection_qualified_name,
-    "qualifiedName": dataset_qualified_name,
-  }))
+    external_topics = [
+        topic_name
+        for topic_name, future in futures.items()
+        if not ((t := future.result()) and t.is_internal)
+    ]
 
-  for field in item["fields"]:
-    elements.append(process_field(field, dataset_qualified_name))
+    return [
+        topic
+        for topic in external_topics
+        if not topic.startswith("_")
+        or topic.endswith(("-offsets", "-status", "-configs"))
+    ]
 
-  return elements
 
-def process_field(field: str, dataset_qualified_name: str) -> DataField:
-  qualified_name = get_qualified_name(field)
+def consume_message(topic: str, consumer: Consumer) -> Union[bytes, None]:
+    """Consume a single message from the provided topic."""
+    try:
+        consumer.subscribe([topic])
+        msg = consumer.poll(5)
+    finally:
+        consumer.unsubscribe()
 
-  return DataField.from_dict({
-      "name": field,
-      "dataset": dataset_qualified_name,
-      "qualifiedName": dataset_qualified_name + "--" + qualified_name,
-      "fieldType": "numeric",
-    })
+    if msg is None or msg.error():
+        return None
 
-async def create_from_kafka(access_token: str, store: ConfigStore):
+    return msg.value()
 
-  # Fetch data from kafka
-  data = discover_cluster(store)
-  # Create Source, System and Collection
-  instances = []
-  system_qualified_name = store.get("system_qualified_name")
-  collection_qualified_name = store.get("collection_qualified_name")
 
-  instances.append(create_source(store.get("data.dictionary.path")))
+def get_message_schema(
+    message: bytes,
+    schema_registry_client: SchemaRegistryClient,
+) -> Union[Schema, None]:
+    if not message.startswith(SCHEMA_REGISTRY_MAGIC_BYTE):
+        logging.warning("Message is not schema-registered")
+        return None
 
-  instances.append(create_system(store.get("system_name"), store.get("system_qualified_name")))
+    try:
+        schema_id = int.from_bytes(message[1:5], "big")
+    except IndexError:
+        logging.error("Message is too short to contain a schema ID")
+        return None
 
-  instances.append(create_collection(store.get("collection_name"), system_qualified_name, collection_qualified_name))
+    try:
+        schema = schema_registry_client.get_schema(schema_id)
+    except SchemaRegistryError:
+        logging.error(f"Failed to retrieve schema with ID {schema_id}")
+        return None
 
-  # Iterate over topics and fields
-  for item in data:
-    instances += process_topic(item, collection_qualified_name)
+    return schema
 
-  for i in instances:
-    atlas_compatible = i.convert_to_atlas()
-    await get_ref_and_push([atlas_compatible], False, access_token)
+
+def build_system(name: str) -> System:
+    return System.from_dict(
+        {
+            "name": name,
+            "qualifiedName": get_qualified_name(name),
+        }
+    )
+
+
+def build_collection(
+    name: str,
+    system_qualified_name: str,
+) -> Collection:
+    return Collection.from_dict(
+        {
+            "name": name,
+            "system": system_qualified_name,
+            "qualifiedName": f"{system_qualified_name}--{get_qualified_name(name)}",
+        }
+    )
+
+
+def build_dataset(topic: str, collection_qualified_name: str) -> Dataset:
+    """Process each topic by creating dataset and field instances."""
+    dataset_qualified_name = (
+        collection_qualified_name + "--" + get_qualified_name(topic)
+    )
+
+    return Dataset.from_dict(
+        {
+            "name": topic,
+            "collection": collection_qualified_name,
+            "qualifiedName": dataset_qualified_name,
+        }
+    )
+
+
+def build_field(name: str, dataset_qualified_name: str, type_name: str) -> DataField:
+    return DataField.from_dict(
+        {
+            "name": name,
+            "dataset": dataset_qualified_name,
+            "qualifiedName": f"{dataset_qualified_name}--{get_qualified_name(name)}",
+            "fieldType": type_name,
+        }
+    )
+
+
+def parse_avro_schema(
+    schema: str,
+    dataset_qualified_name: str,
+) -> Generator[DataField, None, None]:
+    """Parse an Avro schema and yield DataField instances."""
+    avro_schema = loads(schema)
+    for field in avro_schema.fields:
+        yield build_field(
+            name=field.name,
+            dataset_qualified_name=dataset_qualified_name,
+            type_name=field.type.name,
+        )
+
+
+def parse_json_schema(
+    schema: str,
+    dataset_qualified_name: str,
+) -> Generator[DataField, None, None]:
+    """Parse a JSON schema and yield DataField instances."""
+    schema_dict = json.loads(schema)
+    for field, metadata in schema_dict.get("properties", {}).items():
+        yield build_field(
+            name=field,
+            dataset_qualified_name=dataset_qualified_name,
+            type_name=metadata.get("type"),
+        )
+
+
+# Inverted index of data types to standardized names
+DATA_TYPE_MAPPING = {
+    "str": "string",
+    "int": "integer",
+    "float": "number",
+    "bool": "boolean",
+    "dict": "object",
+    "list": "array",
+    "bytes": "binary",
+}
+
+
+def parse_json_payload(
+    payload: str,
+    dataset_qualified_name: str,
+) -> Generator[DataField, None, None]:
+    """Parse a JSON payload and yield DataField instances."""
+    try:
+        data = json.loads(payload)
+    except json.JSONDecodeError:
+        logging.error("Invalid JSON payload")
+        return
+
+    if not isinstance(data, dict):
+        logging.error("JSON payload is not a dictionary")
+        return
+
+    for field, value in data.items():
+        if isinstance(value, dict):
+            type_name = value.get("type", "object")
+        else:
+            type_name = type(value).__name__
+
+        yield build_field(
+            name=field,
+            dataset_qualified_name=dataset_qualified_name,
+            type_name=DATA_TYPE_MAPPING.get(type_name, type_name),
+        )
+
+
+Parser = Callable[[str, str], Generator[DataField, None, None]]
+
+SCHEMA_PARSERS: Dict[str, Parser] = {
+    "avro": parse_avro_schema,
+    "json": parse_json_schema,
+}
+
+
+def discover_cluster(
+    admin_client: AdminClient,
+    consumer: Union[Consumer, None],
+    schema_registry_client: Union[SchemaRegistryClient, None],
+) -> Generator[ToAtlasConvertible, None, None]:
+    """Main function to execute the Kafka topic message consumption process."""
+    system = build_system("kafka_system")
+
+    yield system
+
+    cluster_id = get_cluster_id(admin_client)
+
+    collection = build_collection(
+        cluster_id or "kafka_collection", system.qualified_name
+    )
+
+    yield collection
+
+    datasets = [
+        build_dataset(topic, collection.qualified_name)
+        for topic in get_external_topic_names(admin_client)
+    ]
+
+    yield from datasets
+
+    if not (consumer and schema_registry_client):
+        logging.warning(
+            "No consumer or schema registry client provided. Skipping schema parsing."
+        )
+        return
+
+    for topic in datasets:
+        if not (data := consume_message(topic.name, consumer)):
+            logging.warning(f"No data found for topic: {topic.name}")
+            continue
+
+        if not (schema := get_message_schema(data, schema_registry_client)):
+            logging.info(
+                f"No schema found for topic: {topic.name}. Attempting to parse as JSON payload."
+            )
+
+            yield from parse_json_payload(
+                payload=data.decode("utf-8"),
+                dataset_qualified_name=topic.qualified_name,
+            )
+
+            continue
+
+        if not (parser := SCHEMA_PARSERS.get(schema.schema_type.lower())):
+            logging.warning(
+                f"No parser found for schema type: {schema.schema_type} in topic: {topic.name}"
+            )
+            continue
+
+        yield from parser(schema.schema_str, topic.qualified_name)
+
+
+async def create_from_kafka(
+    admin_client: AdminClient,
+    consumer: Union[Consumer, None],
+    schema_registry_client: Union[SchemaRegistryClient, None],
+    access_token: str,
+):
+    """Scan a Kafka cluster and create Atlas entities from the discovered topics."""
+    for entity in discover_cluster(
+        admin_client=admin_client,
+        consumer=consumer,
+        schema_registry_client=schema_registry_client,
+    ):
+        atlas_compatible = entity.convert_to_atlas()
+        await get_ref_and_push([atlas_compatible], False, access_token)
---
+++ apps/m4i-data-dictionary-io/tests/test__m4i_data_dictionary_io.py
@@ -0,0 +1,371 @@
+from typing import Generator
+
+import pytest
+from confluent_kafka import Consumer, Producer
+from confluent_kafka.admin import AdminClient, NewTopic
+from confluent_kafka.schema_registry import SchemaRegistryClient
+from confluent_kafka.schema_registry.avro import AvroSerializer
+from confluent_kafka.schema_registry.json_schema import JSONSerializer
+from confluent_kafka.serialization import (
+    MessageField,
+    SerializationContext,
+    StringSerializer,
+)
+from m4i_data_dictionary_io.sources.kafka.create_from_kafka import (
+    build_collection,
+    build_dataset,
+    build_field,
+    build_system,
+    discover_cluster,
+)
+from m4i_data_dictionary_io.testing.message import Message
+from tenacity import (
+    Retrying,
+    stop_after_attempt,
+    wait_exponential,
+)
+
+
+def test__discover_cluster_with_empty_cluster(
+    kafka_admin_client: AdminClient,
+    kafka_cluster_id: str,
+    kafka_consumer: Consumer,
+    schema_registry_client: SchemaRegistryClient,
+) -> None:
+    """Test discovering a cluster with an empty Kafka topic."""
+    expected_system = build_system(
+        name="kafka_system",
+    )
+
+    expected_collection = build_collection(
+        name=kafka_cluster_id,
+        system_qualified_name=expected_system.qualified_name,
+    )
+
+    expected = [expected_system, expected_collection]
+
+    discovered = discover_cluster(
+        admin_client=kafka_admin_client,
+        consumer=kafka_consumer,
+        schema_registry_client=schema_registry_client,
+    )
+
+    actual = list(discovered)
+
+    assert expected == actual, f"Expected {expected} but got {actual}"
+
+
+@pytest.fixture()
+def kafka_topic(
+    kafka_admin_client: AdminClient,
+    request: pytest.FixtureRequest,
+) -> Generator[str, None, None]:
+    """Fixture to create a Kafka topic for testing."""
+    topic_name = request.node.name
+
+    futures = kafka_admin_client.create_topics(
+        [NewTopic(topic_name, num_partitions=1, replication_factor=1)]
+    )
+
+    for future in futures.values():
+        if future.exception():
+            raise RuntimeError(f"Failed to create topic {topic_name}")
+        future.result()
+
+    yield topic_name
+
+    kafka_admin_client.delete_topics([topic_name])
+
+
+def test__discover_cluster_with_topic(
+    kafka_admin_client: AdminClient,
+    kafka_cluster_id: str,
+    kafka_consumer: Consumer,
+    kafka_topic: str,
+    schema_registry_client: SchemaRegistryClient,
+) -> None:
+    """Test discovering a cluster with a Kafka topic."""
+    expected_system = build_system(
+        name="kafka_system",
+    )
+
+    expected_collection = build_collection(
+        name=kafka_cluster_id,
+        system_qualified_name=expected_system.qualified_name,
+    )
+
+    expected_dataset = build_dataset(
+        topic=kafka_topic,
+        collection_qualified_name=expected_collection.qualified_name,
+    )
+
+    expected = [expected_system, expected_collection, expected_dataset]
+
+    discovered = discover_cluster(
+        admin_client=kafka_admin_client,
+        consumer=kafka_consumer,
+        schema_registry_client=schema_registry_client,
+    )
+
+    actual = list(discovered)
+
+    assert expected == actual, f"Expected {expected} but got {actual}"
+
+
+@pytest.fixture(scope="module")
+def message() -> Message:
+    """Fixture to create a Message instance for testing."""
+    return Message(
+        content="Test message content",
+        name="test_topic",
+        version=1,
+    )
+
+
+@pytest.fixture()
+def avro_topic(
+    avro_serializer: AvroSerializer,
+    kafka_producer: Producer,
+    kafka_topic: str,
+    message: Message,
+) -> str:
+    """Fixture to create a Kafka topic with an Avro message."""
+    for attempt in Retrying(
+        stop=stop_after_attempt(10),
+        wait=wait_exponential(multiplier=1, min=2, max=10),
+    ):
+        with attempt:
+            value = avro_serializer(
+                obj=message.model_dump(mode="json"),
+                ctx=SerializationContext(kafka_topic, MessageField.VALUE),
+            )
+
+    kafka_producer.produce(topic=kafka_topic, value=value)
+    kafka_producer.poll(0)
+    kafka_producer.flush()
+
+    return kafka_topic
+
+
+def test__discover_cluster_with_topic_and_avro_message(
+    avro_topic: str,
+    kafka_admin_client: AdminClient,
+    kafka_cluster_id: str,
+    kafka_consumer: Consumer,
+    schema_registry_client: SchemaRegistryClient,
+) -> None:
+    """Test discovering a cluster with a Kafka topic that has an Avro message."""
+    expected_system = build_system(
+        name="kafka_system",
+    )
+
+    expected_collection = build_collection(
+        name=kafka_cluster_id,
+        system_qualified_name=expected_system.qualified_name,
+    )
+
+    expected_dataset = build_dataset(
+        topic=avro_topic,
+        collection_qualified_name=expected_collection.qualified_name,
+    )
+
+    expected_fields = [
+        build_field(
+            name="content",
+            dataset_qualified_name=expected_dataset.qualified_name,
+            type_name="string",
+        ),
+        build_field(
+            name="name",
+            dataset_qualified_name=expected_dataset.qualified_name,
+            type_name="string",
+        ),
+        build_field(
+            name="version",
+            dataset_qualified_name=expected_dataset.qualified_name,
+            type_name="long",
+        ),
+    ]
+
+    expected = [
+        expected_system,
+        expected_collection,
+        expected_dataset,
+        *expected_fields,
+    ]
+
+    discovered = discover_cluster(
+        admin_client=kafka_admin_client,
+        consumer=kafka_consumer,
+        schema_registry_client=schema_registry_client,
+    )
+
+    actual = list(discovered)
+
+    assert expected == actual, f"Expected {expected} but got {actual}"
+
+
+@pytest.fixture()
+def json_schema_topic(
+    json_serializer: JSONSerializer,
+    kafka_producer: Producer,
+    kafka_topic: str,
+    message: Message,
+) -> str:
+    """Fixture to create a Kafka topic with a JSON message."""
+    for attempt in Retrying(
+        stop=stop_after_attempt(10),
+        wait=wait_exponential(multiplier=1, min=2, max=10),
+    ):
+        with attempt:
+            value = json_serializer(
+                obj=message.model_dump(mode="json"),
+                ctx=SerializationContext(kafka_topic, MessageField.VALUE),
+            )
+
+    kafka_producer.produce(topic=kafka_topic, value=value)
+    kafka_producer.poll(0)
+    kafka_producer.flush()
+
+    return kafka_topic
+
+
+def test__discover_cluster_with_topic_and_json_schema_message(
+    json_schema_topic: str,
+    kafka_admin_client: AdminClient,
+    kafka_cluster_id: str,
+    kafka_consumer: Consumer,
+    schema_registry_client: SchemaRegistryClient,
+) -> None:
+    """Test discovering a cluster with a Kafka topic that has a JSON message with schema."""
+    expected_system = build_system(
+        name="kafka_system",
+    )
+
+    expected_collection = build_collection(
+        name=kafka_cluster_id,
+        system_qualified_name=expected_system.qualified_name,
+    )
+
+    expected_dataset = build_dataset(
+        topic=json_schema_topic,
+        collection_qualified_name=expected_collection.qualified_name,
+    )
+
+    expected_fields = [
+        build_field(
+            name="content",
+            dataset_qualified_name=expected_dataset.qualified_name,
+            type_name="string",
+        ),
+        build_field(
+            name="name",
+            dataset_qualified_name=expected_dataset.qualified_name,
+            type_name="string",
+        ),
+        build_field(
+            name="version",
+            dataset_qualified_name=expected_dataset.qualified_name,
+            type_name="integer",
+        ),
+    ]
+
+    expected = [
+        expected_system,
+        expected_collection,
+        expected_dataset,
+        *expected_fields,
+    ]
+
+    discovered = discover_cluster(
+        admin_client=kafka_admin_client,
+        consumer=kafka_consumer,
+        schema_registry_client=schema_registry_client,
+    )
+
+    actual = list(discovered)
+
+    assert expected == actual, f"Expected {expected} but got {actual}"
+
+
+@pytest.fixture()
+def json_topic(
+    kafka_producer: Producer,
+    kafka_topic: str,
+    message: Message,
+    string_serializer: StringSerializer,
+) -> str:
+    """Fixture to create a Kafka topic with a JSON message."""
+    for attempt in Retrying(
+        stop=stop_after_attempt(10),
+        wait=wait_exponential(multiplier=1, min=2, max=10),
+    ):
+        with attempt:
+            value = string_serializer(
+                obj=message.model_dump_json(),
+                ctx=SerializationContext(kafka_topic, MessageField.VALUE),
+            )
+
+    kafka_producer.produce(topic=kafka_topic, value=value)
+    kafka_producer.poll(0)
+    kafka_producer.flush()
+
+    return kafka_topic
+
+
+def test__discover_cluster_with_topic_and_json_message(
+    json_topic: str,
+    kafka_admin_client: AdminClient,
+    kafka_cluster_id: str,
+    kafka_consumer: Consumer,
+    schema_registry_client: SchemaRegistryClient,
+) -> None:
+    """Test discovering a cluster with a Kafka topic that has a JSON message without schema."""
+    expected_system = build_system(
+        name="kafka_system",
+    )
+
+    expected_collection = build_collection(
+        name=kafka_cluster_id,
+        system_qualified_name=expected_system.qualified_name,
+    )
+
+    expected_dataset = build_dataset(
+        topic=json_topic,
+        collection_qualified_name=expected_collection.qualified_name,
+    )
+
+    expected_fields = [
+        build_field(
+            name="content",
+            dataset_qualified_name=expected_dataset.qualified_name,
+            type_name="string",
+        ),
+        build_field(
+            name="name",
+            dataset_qualified_name=expected_dataset.qualified_name,
+            type_name="string",
+        ),
+        build_field(
+            name="version",
+            dataset_qualified_name=expected_dataset.qualified_name,
+            type_name="integer",
+        ),
+    ]
+
+    expected = [
+        expected_system,
+        expected_collection,
+        expected_dataset,
+        *expected_fields,
+    ]
+
+    discovered = discover_cluster(
+        admin_client=kafka_admin_client,
+        consumer=kafka_consumer,
+        schema_registry_client=schema_registry_client,
+    )
+
+    actual = list(discovered)
+
+    assert expected == actual, f"Expected {expected} but got {actual}"
