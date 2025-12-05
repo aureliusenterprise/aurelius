07dc133d1dac0c57cab59773eec1febc78e42f15
semnikolaev <sem.nikolaev@aureliusenterprise.com>
2025-12-05 08:01:48 +0000

    fix: split container publishing in steps

--- apps/atlas/src/environments/environment.ts
+++ apps/atlas/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   name: 'm4i_atlas',
   googleAnalyticsMeasurementID: 'UA-138345924-1',
   atlas: {
-    appSearchToken: 'search-6pp14sogy788iyqrb3bhbvon',
+    appSearchToken: 'search-pv68eirjxfnd9ipuqmtmtv86',
   },
   i18n: {
     defaultLanguage: 'en-US',
