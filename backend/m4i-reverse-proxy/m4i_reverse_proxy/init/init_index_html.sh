725c8d66b6b4a0db716170c60be4362f011bf3e6
thijs-franck <thijs.franck@aureliusenterprise.com>
2026-03-16 12:54:36 +0000

    fix: use correct replacement path

--- backend/m4i-reverse-proxy/m4i_reverse_proxy/init/init_index_html.sh
+++ backend/m4i-reverse-proxy/m4i_reverse_proxy/init/init_index_html.sh
@@ -2,11 +2,11 @@
 if [[ -v NAMESPACE ]]; then
     mv /usr/local/apache2/htdocs/index.html /usr/local/apache2/htdocs/index.orig.html
     sed "s/<base href=\"\//<base href=\"\/$NAMESPACE\//g" /usr/local/apache2/htdocs/index.orig.html > /usr/local/apache2/htdocs/index.html
-    
+
     mv /usr/local/apache2/atlas/index.html /usr/local/apache2/atlas/index.orig.html
     sed "s/<base href=\"\//<base href=\"\/$NAMESPACE\/atlas\//g" /usr/local/apache2/atlas/index.orig.html > /usr/local/apache2/atlas/index.html
     #rm -f /usr/local/apache2/atlas/index_.h
-    
+
     #set -- /usr/local/apache2/atlas/main-es*.js
     #mv "$1" /usr/local/apache2/atlas/main-es.h
     if [[ -v ENTERPRISE_SEARCH_INTERNAL_URL ]]; then
@@ -17,10 +17,9 @@ if [[ -v NAMESPACE ]]; then
         cp /usr/local/apache2/atlas/main*.js /usr/local/apache2/bak/
         sed -i "s/atlas:{appSearchToken:\"[a-z0-9\-]*\"/atlas:{appSearchToken:\"${ATLAS_APP_SEARCH_TOKEN}\"/g" /usr/local/apache2/atlas/main*.js
     fi
-    sed -i "s/url:\"\/auth\"/url:\"\/$NAMESPACE\/auth\"/g" /usr/local/apache2/atlas/main*.js
+    sed -i "s/url:\"\/auth\"/url:\"\/$NAMESPACE\"/g" /usr/local/apache2/atlas/main*.js
 
     mkdir /usr/local/apache2/bak2/
     cp /usr/local/apache2/htdocs/main*.js /usr/local/apache2/bak2/
-    sed -i "s/url:\"\/auth\"/url:\"\/$NAMESPACE\/auth\"/g" /usr/local/apache2/htdocs/main*.js
-
+    sed -i "s/url:\"\/auth\"/url:\"\/$NAMESPACE\"/g" /usr/local/apache2/htdocs/main*.js
 fi
