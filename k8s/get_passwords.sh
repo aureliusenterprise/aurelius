1c441cebd5a915d56372642f5f8d71d3ca840034
thijs-franck <thijs.franck@aureliusenterprise.com>
2026-03-25 19:40:52 +0000

    chore: wip

--- k8s/get_passwords.sh
+++ k8s/get_passwords.sh
@@ -8,19 +8,32 @@ echo "keycloak admin:"
 echo "username: ${KC_ADMIN_USERNAME}"
 echo "password: ${KC_ADMIN_PASSWORD}"
 echo "----"
+
 # get keycloak passwords for the default  users of Atlas
-echo "atlas admin user:"
-echo "username: atlas"
-kubectl get secret keycloak-secret-user-admin -o=jsonpath='{.data.password}' -n ${1} | base64 --decode; echo
+ATLAS_ADMIN_USERNAME=$(kubectl get secret keycloak-secret-user-admin -o=jsonpath='{.data.username}' -n ${1} | base64 --decode)
+ATLAS_ADMIN_PASSWORD=$(kubectl get secret keycloak-secret-user-admin -o=jsonpath='{.data.password}' -n ${1} | base64 --decode)
+
+echo "atlas admin:"
+echo "username: ${ATLAS_ADMIN_USERNAME}"
+echo "password: ${ATLAS_ADMIN_PASSWORD}"
 echo "----"
-echo "keycloak Atlas data steward user pwd: "
-echo "username: steward"
-kubectl get secret keycloak-secret-user-steward -o=jsonpath='{.data.password}' -n ${1} | base64 --decode; echo
+
+ATLAS_STEWARD_USERNAME=$(kubectl get secret keycloak-secret-user-steward -o=jsonpath='{.data.username}' -n ${1} | base64 --decode)
+ATLAS_STEWARD_PASSWORD=$(kubectl get secret keycloak-secret-user-steward -o=jsonpath='{.data.password}' -n ${1} | base64 --decode)
+
+echo "atlas steward:"
+echo "username: ${ATLAS_STEWARD_USERNAME}"
+echo "password: ${ATLAS_STEWARD_PASSWORD}"
 echo "----"
-echo "keycloak Atlas data user pwd: "
-echo "username: scientist"
-kubectl get secret keycloak-secret-user-data -o=jsonpath='{.data.password}' -n ${1} | base64 --decode; echo
+
+ATLAS_DATA_SCIENTIST_USERNAME=$(kubectl get secret keycloak-secret-user-data -o=jsonpath='{.data.username}' -n ${1} | base64 --decode)
+ATLAS_DATA_SCIENTIST_PASSWORD=$(kubectl get secret keycloak-secret-user-data -o=jsonpath='{.data.password}' -n ${1} | base64 --decode)
+
+echo "atlas data scientist:"
+echo "username: ${ATLAS_DATA_SCIENTIST_USERNAME}"
+echo "password: ${ATLAS_DATA_SCIENTIST_PASSWORD}"
 echo "=========="
+
 # to get the password to elastic user:
 echo "elasticsearch elastic user pwd: "
 echo "username: elastic"
