8f3a272410218adf3226a6e7cdfb9f86b5ee098a
thijs-franck <thijs.franck@aureliusenterprise.com>
2026-03-31 10:57:02 +0000

    ci: revert docker build caching

--- tools/plugins/docker.ts
+++ tools/plugins/docker.ts
@@ -1,6 +1,5 @@
-import { CreateNodes, CreateNodesResult } from '@nx/devkit';
+import { CreateNodesResult, CreateNodes } from '@nx/devkit';
 import { dirname } from 'path';
-import { execSync } from 'child_process';
 
 export interface DockerPluginOptions {
     readonly buildTargetName?: string;
@@ -16,17 +15,11 @@ export const createNodes: CreateNodes<DockerPluginOptions> = [
     },
 ];
 
-function getBranchName(): string {
-    const branchName = process.env.GITHUB_REF_NAME || execSync('git rev-parse --abbrev-ref HEAD').toString().trim();
-    return branchName.replace(/\//g, '-');
-}
-
 async function createNodesInternal(
     configFilePath: string,
     { buildTargetName = 'docker-build', publishTargetName = 'docker-publish' }: DockerPluginOptions = {},
 ): Promise<CreateNodesResult> {
     const projectRoot = dirname(configFilePath);
-    const branchName = getBranchName();
 
     return {
         projects: {
@@ -34,7 +27,7 @@ async function createNodesInternal(
                 tags: ['docker'],
                 targets: {
                     [buildTargetName]: {
-                        command: `docker buildx build . -f ${configFilePath} -t {projectName}:local --cache-from="type=registry,ref={args.namespace}/{projectName}-cache:main" --cache-from="type=registry,ref={args.namespace}/{projectName}-cache:${branchName}" --cache-to="type=registry,ref={args.namespace}/{projectName}-cache:${branchName},mode=max"`,
+                        command: `docker buildx build . -f ${configFilePath} -t {projectName}:local`,
                         dependsOn: [{ target: 'build' }, { target: buildTargetName, dependencies: true }],
                         metadata: {
                             description: 'Build the Docker image for the application',
@@ -43,11 +36,10 @@ async function createNodesInternal(
                             env: {
                                 DOCKER_BUILDKIT: '1',
                             },
-                            namespace: 'ghcr.io/aureliusenterprise',
                         },
                     },
                     [publishTargetName]: {
-                        command: `docker buildx build . -f ${configFilePath} -t {args.namespace}/{projectName}:{args.version} --push --cache-from="type=registry,ref={args.namespace}/{projectName}-cache:main" --cache-from="type=registry,ref={args.namespace}/{projectName}-cache:${branchName}" --cache-to="type=registry,ref={args.namespace}/{projectName}-cache:${branchName},mode=max"`,
+                        command: `docker buildx build . -f ${configFilePath} -t {args.namespace}/{projectName}:{args.version} --push`,
                         dependsOn: [{ target: 'build' }, { target: buildTargetName, dependencies: true }],
                         metadata: {
                             description: 'Publish the Docker image for the application',
