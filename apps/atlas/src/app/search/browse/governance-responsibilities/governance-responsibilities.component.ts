fcb994440f104c7682f2f1f79d47af7b4f22ceb8
mthomas-aurelius <michalis.thomas@aureliusenterprise.com>
2026-04-14 07:19:04 +0000

    update governance context component for translation support

--- apps/atlas/src/app/search/browse/governance-responsibilities/governance-responsibilities.component.ts
+++ apps/atlas/src/app/search/browse/governance-responsibilities/governance-responsibilities.component.ts
@@ -1,87 +1,82 @@
 import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import {
-  faCompass,
-  faInfoCircle,
-  faProjectDiagram,
-  faSearch,
-  faUser,
-} from '@fortawesome/free-solid-svg-icons';
+import { faCompass, faInfoCircle, faProjectDiagram, faSearch, faUser } from '@fortawesome/free-solid-svg-icons';
 import { AtlasEntitySearchObject } from '@models4insight/atlas/api';
 import { AuthenticationService } from '@models4insight/authentication';
-import {
-  defaultSimpleSearchInputContext,
-  SimpleSearchInputContext,
-} from '@models4insight/components';
+import { defaultSimpleSearchInputContext, SimpleSearchInputContext } from '@models4insight/components';
 import { SearchService } from '../../services/search/search.service';
 import { GovernanceResponsibilitiesInfoModalComponent } from './info-modal/governance-responsibilities-info-modal.component';
+import { TranslateService } from '@ngx-translate/core';
+import { map, Observable } from 'rxjs';
 
 const searchBarContext: SimpleSearchInputContext = {
-  ...defaultSimpleSearchInputContext,
-  label: null,
-  placeholder: 'Search by name or email',
+    ...defaultSimpleSearchInputContext,
+    label: null,
+    placeholder: 'Search by name or email',
 };
 
 @Component({
-  selector: 'models4insight-governance-responsibilities',
-  templateUrl: 'governance-responsibilities.component.html',
-  styleUrls: ['governance-responsibilities.component.scss'],
+    selector: 'models4insight-governance-responsibilities',
+    templateUrl: 'governance-responsibilities.component.html',
+    styleUrls: ['governance-responsibilities.component.scss'],
 })
 export class GovernanceResponsibilitiesComponent {
-  readonly faCompass = faCompass;
-  readonly faInfoCircle = faInfoCircle;
-  readonly faProjectDiagram = faProjectDiagram;
-  readonly faSearch = faSearch;
-  readonly searchBarContext = searchBarContext;
-  readonly faUser = faUser;
+    readonly faCompass = faCompass;
+    readonly faInfoCircle = faInfoCircle;
+    readonly faProjectDiagram = faProjectDiagram;
+    readonly faSearch = faSearch;
+    readonly searchBarContext: Observable<SimpleSearchInputContext>;
+    readonly faUser = faUser;
 
-  @ViewChild(GovernanceResponsibilitiesInfoModalComponent, { static: true })
-  private readonly infoModal: GovernanceResponsibilitiesInfoModalComponent;
+    @ViewChild(GovernanceResponsibilitiesInfoModalComponent, { static: true })
+    private readonly infoModal: GovernanceResponsibilitiesInfoModalComponent;
 
-  constructor(
-    private readonly authenticationService: AuthenticationService,
-    private readonly router: Router,
-    private readonly searchService: SearchService<AtlasEntitySearchObject>
-  ) {}
+    constructor(
+        private readonly authenticationService: AuthenticationService,
+        private readonly router: Router,
+        private readonly searchService: SearchService<AtlasEntitySearchObject>,
+        private readonly translateService: TranslateService,
+    ) {
+        this.searchBarContext = this.translateService
+            .get('search.browse.governanceContext.searchPlaceholder')
+            .pipe(map((translation) => ({ ...searchBarContext, placeholder: translation })));
+    }
 
-  activateInfoModal() {
-    this.infoModal.activate();
-  }
+    activateInfoModal() {
+        this.infoModal.activate();
+    }
 
-  directToPeople() {
-    this.searchService.filters = {
-      all: [{ typename: ['m4i_person'] }],
-    };
+    directToPeople() {
+        this.searchService.filters = {
+            all: [{ typename: ['m4i_person'] }],
+        };
 
-    this.router.navigate(['/search/results']);
-  }
+        this.router.navigate(['/search/results']);
+    }
 
-  onQuerySubmitted(query: string) {
-    this.searchService.query = '';
-    
-    this.searchService.filters = {
-      all: [{ typename: ['m4i_person'] }],
-    };
+    onQuerySubmitted(query: string) {
+        this.searchService.query = '';
 
-    this.router.navigate(['/search/results'], {
-      queryParams: { query },
-    });
-  }
+        this.searchService.filters = {
+            all: [{ typename: ['m4i_person'] }],
+        };
 
-  async directToUser() {
-    const email = await this.authenticationService.get([
-      'credentials',
-      'email',
-    ]);
+        this.router.navigate(['/search/results'], {
+            queryParams: { query },
+        });
+    }
 
-    this.searchService.filters = {
-      all: [{ typename: ['m4i_person'] }],
-    };
+    async directToUser() {
+        const email = await this.authenticationService.get(['credentials', 'email']);
 
-    this.searchService.query = email;
+        this.searchService.filters = {
+            all: [{ typename: ['m4i_person'] }],
+        };
 
-    this.router.navigate(['/search/results'], {
-      queryParams: { query: email },
-    });
-  }
+        this.searchService.query = email;
+
+        this.router.navigate(['/search/results'], {
+            queryParams: { query: email },
+        });
+    }
 }
