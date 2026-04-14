3b9aa5e37a768c4a36bc6da805c4031e1057b115
mthomas-aurelius <michalis.thomas@aureliusenterprise.com>
2026-04-14 07:16:29 +0000

    update business context for translation support

--- apps/atlas/src/app/search/browse/business-context/business-context.component.ts
+++ apps/atlas/src/app/search/browse/business-context/business-context.component.ts
@@ -1,81 +1,72 @@
 import { Component, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
-import {
-  faChartLine,
-  faCompass,
-  faInfoCircle,
-  faProjectDiagram,
-  faSearch
-} from '@fortawesome/free-solid-svg-icons';
-import {
-  AtlasEntitySearchObject
-} from '@models4insight/atlas/api';
-import {
-  defaultSimpleSearchInputContext,
-  SimpleSearchInputContext
-} from '@models4insight/components';
+import { faChartLine, faCompass, faInfoCircle, faProjectDiagram, faSearch } from '@fortawesome/free-solid-svg-icons';
+import { AtlasEntitySearchObject } from '@models4insight/atlas/api';
+import { defaultSimpleSearchInputContext, SimpleSearchInputContext } from '@models4insight/components';
 import { SearchService } from '../../services/search/search.service';
 import { BusinessContextInfoModalComponent } from './info-modal/business-context-info-modal.component';
+import { TranslateService } from '@ngx-translate/core';
+import { map, Observable } from 'rxjs';
 
 const searchBarContext: SimpleSearchInputContext = {
-  ...defaultSimpleSearchInputContext,
-  label: null,
-  placeholder: 'Search for keywords',
+    ...defaultSimpleSearchInputContext,
+    label: null,
+    placeholder: 'Search for keywords',
 };
 
 @Component({
-  selector: 'models4insight-business-context',
-  templateUrl: 'business-context.component.html',
-  styleUrls: ['business-context.component.scss'],
+    selector: 'models4insight-business-context',
+    templateUrl: 'business-context.component.html',
+    styleUrls: ['business-context.component.scss'],
 })
 export class BusinessContextComponent {
-  readonly faChartLine = faChartLine;
-  readonly faCompass = faCompass;
-  readonly faInfoCircle = faInfoCircle;
-  readonly faProjectDiagram = faProjectDiagram;
-  readonly faSearch = faSearch;
-  readonly searchBarContext = searchBarContext;
+    readonly faChartLine = faChartLine;
+    readonly faCompass = faCompass;
+    readonly faInfoCircle = faInfoCircle;
+    readonly faProjectDiagram = faProjectDiagram;
+    readonly faSearch = faSearch;
+    readonly searchBarContext: Observable<SimpleSearchInputContext>;
 
-  @ViewChild(BusinessContextInfoModalComponent, { static: true })
-  private readonly infoModal: BusinessContextInfoModalComponent;
+    @ViewChild(BusinessContextInfoModalComponent, { static: true })
+    private readonly infoModal: BusinessContextInfoModalComponent;
 
-  constructor(
-    private readonly router: Router,
-    private readonly searchService: SearchService<AtlasEntitySearchObject>
-  ) {}
+    constructor(
+        private readonly router: Router,
+        private readonly searchService: SearchService<AtlasEntitySearchObject>,
+        private readonly translateService: TranslateService,
+    ) {
+        this.searchBarContext = this.translateService
+            .get('search.browse.businessContext.searchPlaceholder')
+            .pipe(map((translation) => ({ ...searchBarContext, placeholder: translation })));
+    }
 
-  activateInfoModal() {
-    this.infoModal.activate();
-  }
+    activateInfoModal() {
+        this.infoModal.activate();
+    }
 
-  directToConceptual() {
-    this.searchService.query = '';
+    directToConceptual() {
+        this.searchService.query = '';
 
-    this.searchService.filters = {
-      all: [
-        { sourcetype: ['Business'] },
-        { supertypenames: ['m4i_data_domain'] },
-      ],
-    };
+        this.searchService.filters = {
+            all: [{ sourcetype: ['Business'] }, { supertypenames: ['m4i_data_domain'] }],
+        };
 
-    this.router.navigate(['/search/results']);
-  }
+        this.router.navigate(['/search/results']);
+    }
 
-  directToDashboard() {
-    this.router.navigate(['/dashboard']);
-  }
+    directToDashboard() {
+        this.router.navigate(['/dashboard']);
+    }
 
-  onQuerySubmitted(query: string) {
-    this.searchService.query = query;
+    onQuerySubmitted(query: string) {
+        this.searchService.query = query;
 
-    this.searchService.filters = {
-      all: [
-        { sourcetype: ['Business'] },
-      ],
-    };
+        this.searchService.filters = {
+            all: [{ sourcetype: ['Business'] }],
+        };
 
-    this.router.navigate(['/search/results'], {
-      queryParams: { query },
-    });
-  }
+        this.router.navigate(['/search/results'], {
+            queryParams: { query },
+        });
+    }
 }
