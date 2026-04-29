e28afb7aa10827971aaa79e310059c3eb0cabf2d
mthomas-aurelius <michalis.thomas@aureliusenterprise.com>
2026-04-29 08:18:07 +0000

    translation collection

--- apps/atlas/src/app/search/details/collection/collection-details.module.ts
+++ apps/atlas/src/app/search/details/collection/collection-details.module.ts
@@ -9,18 +9,20 @@ import { CollectionDetailsComponent } from './collection-details.component';
 import { DatasetsCardsComponent } from './datasets-cards/datasets-cards.component';
 import { GovernanceQualityCardsModule } from '../components/governance-quality-cards/governance-quality-cards.module';
 import { DescriptionModule } from '../../components/description/description.module';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    DataQualityListModule,
-    DescriptionModule,
-    DetailsCardsListModule,
-    DetailsNavigationModule,
-    GovernanceQualityCardsModule,
-    PropertiesModule,
-    AccordionModule
-  ],
-  declarations: [DatasetsCardsComponent, CollectionDetailsComponent]
+    imports: [
+        CommonModule,
+        DataQualityListModule,
+        DescriptionModule,
+        DetailsCardsListModule,
+        DetailsNavigationModule,
+        GovernanceQualityCardsModule,
+        PropertiesModule,
+        AccordionModule,
+        TranslateModule,
+    ],
+    declarations: [DatasetsCardsComponent, CollectionDetailsComponent],
 })
 export class CollectionDetailsModule {}
