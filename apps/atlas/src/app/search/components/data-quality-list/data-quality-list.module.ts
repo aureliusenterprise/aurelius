3479d7ea1a119ac25a4fc38f8e5a1049a0766a30
mthomas-aurelius <michalis.thomas@aureliusenterprise.com>
2026-04-29 12:44:33 +0000

    translate data-quality-list

--- apps/atlas/src/app/search/components/data-quality-list/data-quality-list.module.ts
+++ apps/atlas/src/app/search/components/data-quality-list/data-quality-list.module.ts
@@ -1,11 +1,12 @@
 import { CommonModule } from '@angular/common';
-import { NgModule } from "@angular/core";
+import { NgModule } from '@angular/core';
 import { DataQualityPieModule } from '../data-quality-pie/data-quality-pie.module';
 import { DataQualityListComponent } from './data-quality-list.component';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  imports: [CommonModule, DataQualityPieModule],
-  declarations: [DataQualityListComponent],
-  exports: [DataQualityListComponent]
+    imports: [CommonModule, DataQualityPieModule, TranslateModule.forChild()],
+    declarations: [DataQualityListComponent],
+    exports: [DataQualityListComponent],
 })
 export class DataQualityListModule {}
