26e13a5262e618c6c152569b3a7b6d014b4b2840
mthomas-aurelius <michalis.thomas@aureliusenterprise.com>
2026-04-29 13:03:50 +0000

    translate people

--- apps/atlas/src/app/search/components/people/people.module.ts
+++ apps/atlas/src/app/search/components/people/people.module.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { PeopleComponent } from './people.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { TranslateModule } from '@ngx-translate/core';
 
 @NgModule({
-  imports: [CommonModule, FontAwesomeModule],
-  declarations: [PeopleComponent],
-  exports: [PeopleComponent]
+    imports: [CommonModule, FontAwesomeModule, TranslateModule.forChild()],
+    declarations: [PeopleComponent],
+    exports: [PeopleComponent],
 })
 export class PeopleModule {}
