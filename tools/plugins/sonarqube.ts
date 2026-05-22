54f6bd210f25c2d4550060746473c473646d310f
thijs-franck <thijs.franck@aureliusenterprise.com>
2026-05-22 04:08:28 +0000

    ci: setup sonarqube and add first projects

---
+++ tools/plugins/sonarqube.ts
@@ -0,0 +1,41 @@
+import { CreateNodes, CreateNodesResult } from '@nx/devkit';
+import { dirname } from 'node:path';
+
+export interface SonarQubePluginOptions {
+    readonly sonarTargetName?: string;
+}
+
+const glob = '**/sonar-project.properties';
+
+export const createNodes: CreateNodes<SonarQubePluginOptions> = [
+    glob,
+    async (configFilePath, options) => {
+        return createNodesInternal(configFilePath, options);
+    },
+];
+
+async function createNodesInternal(
+    configFilePath: string,
+    { sonarTargetName = 'sonar' }: SonarQubePluginOptions = {},
+): Promise<CreateNodesResult> {
+    const projectRoot = dirname(configFilePath);
+
+    return {
+        projects: {
+            [projectRoot]: {
+                targets: {
+                    [sonarTargetName]: {
+                        dependsOn: ['decrypt'],
+                        executor: 'nx:run-commands',
+                        metadata: {
+                            description: 'Run SonarQube analysis on the project',
+                        },
+                        options: {
+                            command: `sonar-scanner -Dproject.settings=${projectRoot}/sonar-project.properties -Dsonar.working.directory=${projectRoot}/.scannerwork`,
+                        },
+                    },
+                },
+            },
+        },
+    };
+}
