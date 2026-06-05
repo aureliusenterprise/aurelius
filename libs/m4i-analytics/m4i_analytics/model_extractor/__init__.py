dc1ba2f93789d8eca10652b4d1bb9f37a0e37eda
thijs-franck <thijs.franck@aureliusenterprise.com>
2026-06-05 09:35:07 +0000

    fix: remove unused MonitoringReactive import from model_extractor

--- libs/m4i-analytics/m4i_analytics/model_extractor/__init__.py
+++ libs/m4i-analytics/m4i_analytics/model_extractor/__init__.py
@@ -1,7 +1,6 @@
 from m4i_analytics.model_extractor.ExtractorLanguagePrimitives import ExtractorLanguagePrimitives
 from m4i_analytics.model_extractor.Monitoring import Monitoring
-from m4i_analytics.model_extractor.MonitoringReactive import Monitoring as MonitoringReactive
 from m4i_analytics.model_extractor.NifiExtractor import NifiExtractor
 from m4i_analytics.model_extractor.Pathing import Pathing
 
-__all__ = ["ExtractorLanguagePrimitives", "Monitoring", "MonitoringReactive", "NifiExtractor", "Pathing"]
+__all__ = ["ExtractorLanguagePrimitives", "Monitoring", "NifiExtractor", "Pathing"]
