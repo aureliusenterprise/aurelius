18377f59f321c8d8d583853d1d1073b94cd99c20
mthomas-aurelius <michalis.thomas@aureliusenterprise.com>
2026-07-03 12:27:15 +0000

    parametrized modelview2 and some other files

--- apps/atlas/src/app/search/details/components/properties/properties.component.ts
+++ apps/atlas/src/app/search/details/components/properties/properties.component.ts
@@ -1,28 +1,32 @@
 import { Component, OnInit } from '@angular/core';
 import { SortableTableShellConfig } from '@models4insight/components';
+import { TranslateService } from '@ngx-translate/core';
 import { Observable } from 'rxjs';
 import { DataForTable, PropertiesService } from './properties.service';
 
-const tableConfigProperties: SortableTableShellConfig<DataForTable> = {
-  name: { displayName: 'Key', isNarrow: true },
-  value: { displayName: 'Value', isNarrow: true }
-};
+const createTableConfigProperties = (translateService: TranslateService): SortableTableShellConfig<DataForTable> => ({
+    name: { displayName: translateService.instant('search.details.properties.name'), isNarrow: true },
+    value: { displayName: translateService.instant('search.details.properties.value'), isNarrow: true },
+});
 
 @Component({
-  selector: 'models4insight-properties',
-  templateUrl: './properties.component.html',
-  styleUrls: ['./properties.component.scss'],
-  providers: [PropertiesService]
+    selector: 'models4insight-properties',
+    templateUrl: './properties.component.html',
+    styleUrls: ['./properties.component.scss'],
+    providers: [PropertiesService],
 })
 export class PropertiesComponent implements OnInit {
-  readonly tableConfigProperties = tableConfigProperties;
+    readonly tableConfigProperties: SortableTableShellConfig<DataForTable>;
 
-  dataForTable$: Observable<DataForTable[]>;
-  constructor(
-    private readonly propertiesService: PropertiesService
-  ) { }
+    dataForTable$: Observable<DataForTable[]>;
+    constructor(
+        private readonly propertiesService: PropertiesService,
+        private readonly translateService: TranslateService,
+    ) {
+        this.tableConfigProperties = createTableConfigProperties(translateService);
+    }
 
-  ngOnInit() {
-    this.dataForTable$ = this.propertiesService.select('propertiesList');
-  }
+    ngOnInit() {
+        this.dataForTable$ = this.propertiesService.select('propertiesList');
+    }
 }
